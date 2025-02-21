8bde9d8301ee616ff2ebf1099d5ed02e2b3d95db
Yann MOUEIX <57915257+GraphhCODE@users.noreply.github.com>
2025-02-21 10:46:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import TableComponent from "../components/TableComponent";
 export default function Home() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Tableau des Subventions</h1>
+      <h1 className="text-2xl font-bold mb-4">Aides CARSAT</h1>
       <TableComponent />
     </div>
   );
